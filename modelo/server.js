280e59c0ef8d58161d8c9a6d82a240415dc8f8de
DonPablo0 <92349192+DonPablo0@users.noreply.github.com>
2022-11-17 12:56:15 -0500

    get-post

--- modelo/server.js
+++ modelo/server.js
@@ -6,7 +6,7 @@ class Server {
 
     constructor() {
         this.app = express()
-        this.port = process.env.port
+        this.port = process.env.PORT
         this.prestamosPath = '/api/prestamo'
         this.herramientasPath = '/api/herramienta'
         this.conectardb()
